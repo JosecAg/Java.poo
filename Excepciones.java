e8bd039f6f03bfe6bef9d516b590899a15d502d4
Jose Araujo <89282572+JosecAg@users.noreply.github.com>
2022-09-22 18:02:35 -0500

    Add files via upload

---
+++ Excepciones.java
@@ -0,0 +1,24 @@
+package poo3.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Excepciones {
+    public static void main(String[] args) throws IOException {
+        BufferedReader leer=new BufferedReader(new InputStreamReader(System.in));
+        float a, b, r;
+        try{
+            System.out.println("Ingrese un valor: ");
+            a = Float.parseFloat(leer.readLine());
+            System.out.println("Ingresar un valor: ");
+            b = Float.parseFloat(leer.readLine());
+
+            r=a/b;
+
+            System.out.println("La division es: "+r);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
